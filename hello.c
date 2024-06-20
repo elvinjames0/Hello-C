0161ed8e29031c634e237383b55a21c2e8deebde
elvinjames0 <huythanh022302@gmail.com>
2024-06-20 23:57:50 +0700

    learn how to declaration and initialization a variable

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+include là lệnh tiền xử lý báo cho trình biên dịch nội dung của tập tin
+*/
+int main(){
+printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+   return 0; // trả về trạng thái thoát của chương trình, trả về 0 nếu nó true và trả về 1 nếu false
+} 
+
