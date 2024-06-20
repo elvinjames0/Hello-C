eb61bbdf11907727cc9c763548c7a919714f7e52
elvinjames0 <huythanh022302@gmail.com>
2024-06-21 00:06:16 +0700

    learn how to declaration and initialization a variable

--- hello.c
+++ hello.c
@@ -3,7 +3,15 @@
 include là lệnh tiền xử lý báo cho trình biên dịch nội dung của tập tin
 */
 int main(){
-printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    int x;
+    x = 123;
+    float gpa = 9.9;
+    char grade = 'A';
+    char name[] = "Huy Thành";
+    printf("%d\n", x);
+    printf("%f\n", gpa);
+    printf("%c\n", grade);
+    printf("%s\n", name);
    return 0; // trả về trạng thái thoát của chương trình, trả về 0 nếu nó true và trả về 1 nếu false
 } 
 
